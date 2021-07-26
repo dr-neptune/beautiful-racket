db3fee9569037eb4ac8a72e9ab3b247f8cdec396
dr-neptune <mrose4@worcester.edu>
2021-07-26 19:14:08 -0400

    follow the grammar

--- bf.rkt
+++
@@ -1,6 +0,0 @@
-; > | increase the pointer position by one
-; < | decrease the pointer position by one
-; + | increase the value of the current byte by one
-; - | decrease the value of the current byte by one
-; . | write the current byte to stdout
-; , | read a byte from stdin and store it in the current byte (overwriting the existing value)
