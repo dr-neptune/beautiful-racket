48c16aa1f74a1f05147fe48e4010e474b80a98f3
dr-neptune <mrose4@worcester.edu>
2021-07-31 12:33:39 -0400

    microphone check 1 2

--- jsonic/contract-test.rkt
+++ jsonic/contract-test.rkt
@@ -1,3 +1,4 @@
+#lang racket
 (module our-submod br
   (require racket/contract)
   (define (our-div num denom)
@@ -8,4 +9,4 @@
             [out-div (number? (not/c zero?) . -> . number?)])))
 
 (require (submod "." our-submod))
-(our-div 42 0)
+(our-div 42 6)
--- jsonic/reader.rkt
+++ jsonic/reader.rkt
@@ -1,8 +1,11 @@
 #lang br/quicklang
-(require "tokenizer.rkt" "parser.rkt")
+(require "tokenizer.rkt" "parser.rkt" racket/contract)
 
+; takes any for path and a port for port and returns syntax
 (define (read-syntax path port)
   (define parse-tree (parse path (make-tokenizer port)))
   (define module-datum `(module jsonic-module jsonic/expander ,parse-tree))
   (datum->syntax #f module-datum))
-(provide read-syntax)
+
+(provide (contract-out
+          [read-syntax (any/c input-port? . -> . syntax?)]))
