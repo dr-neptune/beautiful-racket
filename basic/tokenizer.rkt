33977f51c4ad2b78b101b70146f78308a80ccfdf
dr-neptune <mrose4@worcester.edu>
2021-08-12 21:14:11 -0400

    fairweather friends

--- basic/tokenizer.rkt
+++ basic/tokenizer.rkt
@@ -1,22 +1,10 @@
 #lang br
-(require brag/support)
+(require "lexer.rkt" brag/support)
 
-(define-lex-abbrev digits (:+ (char-set "0123456789")))
+(define (make-tokenizer ip [path #f])
+  (port-count-lines! ip)
+  (lexer-file-path path)
+  (define (next-token) (basic-lexer ip))
+  next-token)
 
-(define basic-lexer
-  (lexer-srcloc
-   ["\n" (token 'NEWLINE lexeme)]                       ; handle newlines
-   [whitespace (token lexeme #:skip? #t)]               ; handle whitespace
-   [(from/stop-before "rem" "\n") (token 'REM lexeme)]  ; handle comments "rem"
-   [(:or "print" "goto" "end" "+" ":" ";")              ; handle parsing operators
-    (token lexeme lexeme)]
-   [digits (token 'INTEGER (string->number lexeme))]    ; handle a single digit
-   [(:or (:seq (:? digits) "." digits)                  ; handle compound digit expressions
-         (:seq digits "."))
-    (token 'DECIMAL (string->number lexeme))]
-   [(:or (from/to "\""  "\"") (from/to "'" "'"))        ; handle strings
-    (token 'STRING
-           (substring lexeme
-                      1 (sub1 (string-length lexeme))))]))
-
-(provide basic-lexer)
+(provide make-tokenizer)
